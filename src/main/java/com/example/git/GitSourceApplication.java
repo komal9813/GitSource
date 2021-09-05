f1ef7a3e82ef42e22ec5cfe6fcd81134ef5b132d
komal <komal98singh19@gmail.com>
2021-09-05 21:32:36 +0530

    GitSource Assignment initial commit

---
+++ src/main/java/com/example/git/GitSourceApplication.java
@@ -0,0 +1,14 @@
+package com.example.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSourceApplication.class, args);
+		System.out.println("GitSource Demo ");
+	}
+
+}
